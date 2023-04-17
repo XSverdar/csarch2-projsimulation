ffa6f56630f367174b40f1d2330d2b923f0d2a6f
Erin <57384457+XSverdar@users.noreply.github.com>
2023-04-17 22:01:13 +0800

    Add files via upload

---
+++ DecimalToSignedBinary.py
@@ -0,0 +1,26 @@
+from twosComp import twosComp
+
+def solve(decimal):
+    binary = ['0'] # initialize list
+    nums = '' # empty string
+
+    if decimal < 0: # input is negative
+        decToBin(abs(decimal), binary) # get positive of input
+        binary = twosComp(binary)
+
+    else: # input is positive
+        decToBin(decimal, binary)
+        
+    for i in binary:
+        nums += str(i)
+
+    return nums # return list as string
+
+def decToBin(num, binary):
+    if num >= 1: # to not exceed maximum recursion depth
+        decToBin(num//2, binary)
+        binary.append(str(num%2))
+
+print("Enter decimal: ")
+x = int(input("Enter decimal: "))
+print(solve(x))
