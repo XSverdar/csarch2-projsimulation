c83ca37c6df23ed73705dc1b2496bbaad54c2f7a
XSverdar <hythunderz@gmail.com>
2023-04-17 22:15:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import DecimalToSignedBinary
 import pencilAndPaper
 import booths
+import extendedBooths
 
 def main():
-    inp1 = input("Choose a method between pencil and paper (1) or Booth's method (2): ")
+    print("Choose a method:\n")
+    print("[1] Pencil and Paper\n[2] Booth's\n[3] Extended Booth's\n[4] All of the Above")
+    inp1 = input()
+
     if(inp1 == "1"):
         print("Pencil and Paper\n")
         inp = input("Choose input type (D/B): ")
@@ -13,7 +17,6 @@ def main():
             pencilAndPaper.solve(decimal1, decimal2)
         elif(inp == 'B'):
             pencilAndPaper.get()
-
     
     elif(inp1 == "2"):
         print("Booth's\n")
@@ -24,7 +27,30 @@ def main():
             booths.solve(decimal1, decimal2)
         elif(inp == 'B'):
             booths.get()
-    
+
+    elif(inp1 == "3"):
+        print("Extended Booth's\n")
+        inp = input("Choose input type (D/B): ")
+        if(inp == 'D'):
+            decimal1 = DecimalToSignedBinary.inputs()
+            decimal2 = DecimalToSignedBinary.inputs()
+            extendedBooths.solve(decimal1, decimal2)
+        elif(inp == 'B'):
+            binary1 = input("Enter multiplicand: ")
+            binary2 = input("Enter multiplier: ")
+            extendedBooths.solve(binary1, binary2)
+
+    elif(inp1 == "4"):
+        print("All of the Above\n")
+        inp = input("Choose input type (D/B): ")
+        if(inp == 'D'):
+            decimal1 = DecimalToSignedBinary.inputs()
+            decimal2 = DecimalToSignedBinary.inputs()
+            extendedBooths.solve(decimal1, decimal2)
+        elif(inp == 'B'):
+            binary1 = input("Enter multiplicand: ")
+            binary2 = input("Enter multiplier: ")
+            extendedBooths.solve(binary1, binary2)
     loop()
 
 def loop():
